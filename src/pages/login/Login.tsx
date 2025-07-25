798eb22850511da5b183a6dfd2f297192b4f114a
Rodrigo Aguillar <flavioaguilar2001@gmail.com>
2025-07-25 15:42:56 -0300

    feat: adiciona exercício com props, hooks e renderização condicional

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,7 +1,41 @@
+import { useState } from "react";
+import Home from "../home/Home";
+
 function Login() {
-  return (
-    <div>Login</div>
-  )
+
+    const [isLogged, setIsLogged] = useState(false);
+
+    return (
+        <>
+            {
+                isLogged ? (
+                    <Home
+                        titulo="Componente Home"
+                        texto="Bem-vindo de Volta!"
+                    />
+                ) : (
+                    <div>
+                        <h2>Componente Login</h2>
+                        <button onClick={() => setIsLogged(true)}>Entrar</button>
+                    </div>
+                )
+            }
+        </>
+    )
 }
 
 export default Login
+
+
+/* useState (linha 6) cria o estado inicial da variável isLogged como false.
+   setIsLogged é a função que altera esse valor. */
+
+/* O onClick chama uma arrow function que, quando executada,
+   altera o estado com setIsLogged(true) (linha 19). */
+
+/* setIsLogged(true) atualiza o valor de isLogged para true.
+   Isso faz o componente re-renderizar (desconstruir a const) com base no novo estado. */
+
+/* A renderização condicional com isLogged ? e :  funciona
+   como um if/else: se isLogged for true, mostra <Home />, senão
+   mostra o conteúdo de login (linhas 13 a 20). */
--- src/pages/tarefa/Tarefa.tsx
+++ src/pages/tarefa/Tarefa.tsx
@@ -1,8 +1,52 @@
-﻿
-function Tarefa() {
+﻿import {useState, useEffect} from "react";
+
+function Tarefa (){
+
+  const [completed, setCompleted] = useState (false);
+  const [tarefa, setTarefa] = useState ('');
+
+  useEffect(()=> {
+    if (completed) {
+      setTarefa ('Parabéns! Você concluiu a tarefa!');
+    }
+  }, [completed]);
+
   return (
-    <div>Componente Tarefa</div>
+    <div>
+      <h2>Componente Tarefa</h2>
+      <h3>{tarefa}</h3>
+      <p>Conclua a tarefa</p>
+      <button onClick={() => setCompleted (true)}>Concluir Tarefa</button>
+    </div>
   )
 }
 
 export default Tarefa
+
+
+/* Linha 5: useState cria o estado "completed" com valor inicial false.
+   "setCompleted" é a função usada para mudar esse valor. */
+
+/* Linha 19: Quando o usuário clicar no botão, a arrow function será executada.
+   Ela chama setCompleted(true), atualizando o estado para verdadeiro. */
+
+/* "setCompleted(true)" muda o valor de "completed" para true.
+   Isso dispara o useEffect porque estamos observando "completed". */
+
+/* Linha 8: useEffect é uma função do React que roda após renderizações (atualizações, nesse caso na completed).
+   Ele é configurado para reagir apenas quando "completed" mudar (linha 12). 
+   Se completed sofre alteração, passar a ser true, o useEffect executa setTarefa com a mensagem da linha 10. */
+
+
+/* O useEffect é executado sempre que o valor de "completed" mudar, porque ele está listado no array de dependências [completed] (linha 12).
+
+   No entanto, se a gente quiser que uma ação (como mostrar a mensagem "Parabéns!") aconteça apenas quando "completed" for true,
+   é importante usar um if (completed) dentro do useEffect.
+
+   Sem o if, o código dentro do useEffect rodaria também quando "completed" for false, o que pode causar comportamentos indesejados
+   
+   (como apagar ou sobrescrever a mensagem sem necessidade, se o estado mudar de volta para false futuramente).
+
+   Ou seja, o if garante que a ação só será executada quando completed for exatamente true. */
+
+
